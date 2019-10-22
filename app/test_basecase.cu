4b37efd38465d5895e2a02a57a9688577473f1b0
Tobias Ribizel <oss@upsj.de>
2019-10-22 14:23:01 +0200

    add licenses, license headers and README

--- app/test_basecase.cu
+++ app/test_basecase.cu
@@ -1,3 +1,19 @@
+/*
+ * Parallel selection algorithm on GPUs
+ * Copyright (c) 2018-2019 Tobias Ribizel (oss@ribizel.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "catch.hpp"
 #include "../lib/utils_basecase.cuh"
 #include "test_fixture.cuh"
